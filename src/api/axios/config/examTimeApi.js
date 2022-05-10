ff1fee5ed7856ee27cfdb29fde2e2fad93f6d73c
SauLuoiBo2 <101506269+SauLuoiBo2@users.noreply.github.com>
2022-05-10 17:31:04 +0700

    sadasd

---
+++ src/api/axios/config/examTimeApi.js
@@ -0,0 +1,20 @@
+import axiosClient from '../axiosClient';
+
+class ExamTimeApi {
+    constructor() {
+        this.api = 'time-type/';
+    }
+    search = () => {
+        const url = `${this.api}search`;
+        const body = {
+            keyword: '',
+            page: 1,
+            limit: 32,
+        };
+        return axiosClient.post(url, body);
+    };
+}
+
+const examTimeApi = new ExamTimeApi();
+
+export { examTimeApi };
